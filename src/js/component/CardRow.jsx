fb9277f112bf3066552e333b8b8fe650642224e1
rick-rodriguez16 <rickrod6502@gmail.com>
2024-10-12 00:33:40 +0000

    updated Navbar, Jumbotron, CardRow, Card components

--- src/js/component/CardRow.jsx
+++ src/js/component/CardRow.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Card from "./Card";
 
 const CardRow = () => {
 
     return (
         <>
+            <div className="d-flex">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </>
     );
 }
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import CardRow from "./CardRow";
+import Footer from "./Footer";
+
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-			
+			<Navbar />
+			<div className="row">
+				<div className="col-2"></div>
+				<div className="col-8">
+					<Jumbotron />
+				</div>
+				<div className="col-2"></div>
+			</div>
+			<div className="row">
+				<div className="col-2"></div>
+				<div className="col-8">
+					<CardRow />
+				</div>
+				<div className="col-2"></div>
+			</div>
+			<Footer />
 		</>
 	);
 };
